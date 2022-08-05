fcb4854d7697aea188fa1dccdfd6980e656441a1
Dmitry L <ds-sev@ya.ru>
2022-08-05 10:01:18 +0300

    add format for Task 2

--- Task2_days_of_week/Program.cs
+++ Task2_days_of_week/Program.cs
@@ -5,36 +5,14 @@
 
 Console.WriteLine("Введите номер дня недели: ");
 int numOfDay = Convert.ToInt32(Console.ReadLine());
-if (numOfDay == 1)
-{
-    Console.WriteLine("Понедельник ");
-}
-else if (numOfDay == 2)
-{
-    Console.WriteLine("Вторник ");
-}
-else if (numOfDay == 3)
-{
-    Console.WriteLine("Среда ");
-}
-else if (numOfDay == 4)
-{
-    Console.WriteLine("Четверг ");
-}
-else if (numOfDay == 5)
-{
-    Console.WriteLine("Пятница ");
-}
-else if (numOfDay == 6)
-{
-    Console.WriteLine("Суббота ");
-}
-else if (numOfDay == 7)
-{
-    Console.WriteLine("Воскресенье ");
-}
-else
-{
-    Console.WriteLine("Необходимо ввести число в диапазоне от 1 до 7. ");
-}
+
+if (numOfDay == 1) Console.WriteLine("Понедельник ");
+else if (numOfDay == 2) Console.WriteLine("Вторник ");
+else if (numOfDay == 3) Console.WriteLine("Среда ");
+else if (numOfDay == 4) Console.WriteLine("Четверг ");
+else if (numOfDay == 5) Console.WriteLine("Пятница ");
+else if (numOfDay == 6) Console.WriteLine("Суббота ");
+else if (numOfDay == 7) Console.WriteLine("Воскресенье ");
+else Console.WriteLine("Необходимо ввести число в диапазоне от 1 до 7. ");
+
 
