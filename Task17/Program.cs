9a07c0280e8a774c0043f89acc1c15c3b7360e44
Dmitry L <ds-sev@ya.ru>
2022-08-11 20:47:08 +0300

    add Task 17 Method

--- Task17/Program.cs
+++ Task17/Program.cs
@@ -11,10 +11,31 @@ int x = Convert.ToInt32(Console.ReadLine());
 Console.Write("Y: ");
 int y = Convert.ToInt32(Console.ReadLine());
 
-if ( x > 0 && y > 0 ) Console.WriteLine("Первая четверть");
-else if ( x < 0 && y > 0 ) Console.WriteLine("Вторая четверть");
-else if ( x < 0 && y < 0 ) Console.WriteLine("Третья четверть");
-else if ( x > 0 && y < 0 ) Console.WriteLine("Четвертая четверть");
-else Console.WriteLine("Введены некорректные координаты");
+// if ( x > 0 && y > 0 ) Console.WriteLine("Первая четверть");
+// else if ( x < 0 && y > 0 ) Console.WriteLine("Вторая четверть");
+// else if ( x < 0 && y < 0 ) Console.WriteLine("Третья четверть");
+// else if ( x > 0 && y < 0 ) Console.WriteLine("Четвертая четверть");
+// else Console.WriteLine("Введены некорректные координаты");
+
+string GetQuarter (int xc, int yc)
+{
+    if ( xc > 0 && yc > 0 ) return "Первая четверть";
+    if ( xc < 0 && yc > 0 ) return "Вторая четверть";
+    if ( xc < 0 && yc < 0 ) return "Третья четверть";
+    if ( xc > 0 && yc < 0 ) return "Четвертая четверть";
+    return "Введены некорректные координаты";
+}
+
+string result = GetQuarter(x, y);
+Console.WriteLine(result);
+
+
+
+
+
+
+
+
+
 
 
